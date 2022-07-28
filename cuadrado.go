8a7eb46f1b21e07ccc9b4b7952b366d3ece30ae6
Harry Alvarado 😎 <harry2alvarado8@hotmail.com>
2022-07-27 22:36:03 -0500

    Tipos de datos primitivos

--- cuadrado.go
+++ cuadrado.go
@@ -34,4 +34,32 @@ func main() {
 	areaCuadrado := baseCuadrado * baseCuadrado
 
 	fmt.Println("El área del cuadrado es:", areaCuadrado)
+
+	//Numeros enteros
+	//int = Depende del OS (32 o 64 bits)
+	//int8 = 8bits = -128 a 127
+	//int16 = 16bits = -2^15 a 2^15-1
+	//int32 = 32bits = -2^31 a 2^31-1
+	//int64 = 64bits = -2^63 a 2^63-1
+
+	//Optimizar memoria cuando sabemos que el dato simpre va ser positivo
+	//uint = Depende del OS (32 o 64 bits)
+	//uint8 = 8bits = 0 a 127
+	//uint16 = 16bits = 0 a 2^15-1
+	//uint32 = 32bits = 0 a 2^31-1
+	//uint64 = 64bits = 0 a 2^63-1
+
+	//numeros decimales
+	// float32 = 32 bits = +/- 1.18e^-38 +/- -3.4e^38
+	// float64 = 64 bits = +/- 2.23e^-308 +/- -1.8e^308
+
+	//textos y booleanos
+	//string = ""
+	//bool = true or false
+
+	//numeros complejos
+	//Complex64 = Real e Imaginario float32
+	//Complex128 = Real e Imaginario float64
+	//Ejemplo : c:=10 + 8i
+
 }
